8ffe355e85de4994753f76df94dd92115b6891ab
Alok <108712328+Krishan-Kant123@users.noreply.github.com>
2025-02-07 20:33:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,13 @@ app.add_middleware(
 
 
 
-HEADERS = {"User-Agent": "Mozilla/5.0"}
+HEADERS = {
+     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Referer": "https://www.google.com/",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+}
 
 
 def scrape_pagalfree_search(query: str):
